e6644cc8cbd20eea62a8b9966d51d64609b73dbc
Martijn Hols <martijnhols@hotmail.com>
2018-02-12 23:02:19 +0100

    Run a setup similar to the spec parsers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import fs from 'fs';
 import readline from 'readline';
 
-const TIME_SEPARATOR = '  ';
-const ENCOUNTER_START = 'ENCOUNTER_START';
-
 const entities = {};
 
 function processEntity(parts, startIndex) {
@@ -18,13 +15,56 @@ function processEntity(parts, startIndex) {
 }
 
 class CombatLogParser {
-  lineNo = null;
+  static TIME_SEPARATOR = '  ';
+
   time = null;
+  lineNo = null;
 
-  on_event(lineNo, time, event) {
-    this.lineNo = lineNo;
-    this.time = time;
+  _path = null;
+  constructor(path) {
+    this._path = path;
+  }
 
+  start() {
+    return new Promise((resolve, reject) => {
+      const rl = readline.createInterface({
+        input: fs.createReadStream(this._path),
+        crlfDelay: Infinity
+      });
+      // TODO: Read from index file to determine how many lines we can skip
+
+      this.lineNo = 0;
+      const timeSeparatorLength = this.constructor.TIME_SEPARATOR.length;
+      this.on_start();
+      rl.on('line', line => {
+        this.lineNo += 1;
+        const timeEndIndex = line.indexOf(this.constructor.TIME_SEPARATOR);
+        this.time = this._convertTime(line.substr(0, timeEndIndex));
+        const eventIndex = timeEndIndex + timeSeparatorLength;
+        const event = line.substr(eventIndex);
+        const eventParts = event.split(',');
+        this.on_event(eventParts);
+      });
+      rl.on('close', () => {
+        this.on_finish();
+        resolve(this.lineNo);
+      });
+    });
+  }
+  _convertTime(time) {
+    // TODO: Convert time into a date
+    return time;
+  }
+
+  on_start() {
+    console.time('parse');
+  }
+  on_finish() {
+    console.timeEnd('parse');
+    console.log('Read entire file.', this.lineNo, 'lines');
+  }
+
+  on_event(event) {
     const eventName = event[0];
 
     const startIndex = 1;
@@ -36,18 +76,20 @@ class CombatLogParser {
     // const someFlag = parts[11];
     // const buffType = parts[12];
 
-    // TODO: Just use reflection to call methods automatically and if one event isn't supported have a clean fallback that tries to figure it out and still sends the data
-    switch (eventName) {
-      case ENCOUNTER_START:
-        this.on_ENCOUNTER_START(event, startIndex);
-        break;
-      case 'SPELL_CAST_SUCCESS':
-        break;
-      case 'SPELL_AURA_REFRESH':
-        break;
-      default: break;
+    const eventHandlerName = this._eventHandlerName(eventName);
+    this._callMethod(eventHandlerName, event, startIndex);
+    // TODO: Handle when the event handler doesn't exist so that no data gets lost
+  }
+  _eventHandlerName(eventType) {
+    return `on_${eventType}`;
+  }
+  _callMethod(methodName, ...args) {
+    const method = this[methodName];
+    if (method) {
+      method.call(this, ...args);
     }
   }
+
   on_ENCOUNTER_START(parts, startIndex) {
     const bossId = parts[startIndex + 0];
     const bossName = parts[startIndex + 1];
@@ -55,28 +97,15 @@ class CombatLogParser {
     const players = parts[startIndex + 3];
 
     console.log(this.lineNo, this.time, bossId, bossName, difficulty, players);
+    // TODO: Write this to an index file
+    // TODO: Write last line parsed to an index file
   }
 }
 
-console.time('parse');
-const rl = readline.createInterface({
-  input: fs.createReadStream('WoWCombatLog.txt'),
-  crlfDelay: Infinity
-});
-let lineNr = 0;
-const timeSeparatorLength = TIME_SEPARATOR.length;
-const parser = new CombatLogParser();
-rl.on('line', line => {
-  lineNr += 1;
-  const timeEndIndex = line.indexOf(TIME_SEPARATOR);
-  const time = line.substr(0, timeEndIndex); // TODO: Parse this, pass this
-  const eventIndex = timeEndIndex + timeSeparatorLength;
-  const event = line.substr(eventIndex);
-  const eventParts = event.split(',');
-  parser.on_event(lineNr, time, eventParts);
-});
-rl.on('close', () => {
-  console.timeEnd('parse');
-  console.log('Read entire file.', lineNr, 'lines');
-  process.exit(0);
-});
+const parser = new CombatLogParser('WoWCombatLog.txt');
+parser.start()
+  .then(results => {
+    process.exit(0);
+  }, error => {
+    process.exit(0);
+  });
