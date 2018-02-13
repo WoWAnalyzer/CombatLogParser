3380f5898c16523d20954b0230923c4d84d999f7
Martijn Hols <martijnhols@hotmail.com>
2018-02-13 23:52:51 +0100

    Unescape strings properly Handle escaped quotes properly

--- src/CombatLogFileReader.js
+++ src/CombatLogFileReader.js
@@ -35,6 +35,11 @@ class CombatLogFileReader extends EventEmitter {
             // If the previous character was anything other than a quote we're still in the middle of the string.
             continue; // next character
           }
+          // Ok so previous character was a quote, but was that quote escaped? Because if it was then we're still in the middle of the string.
+          const isEscaped = line[i - 2] === '\\';
+          if (isEscaped) {
+            continue;
+          }
         }
 
         // End of part
@@ -47,7 +52,11 @@ class CombatLogFileReader extends EventEmitter {
         }
         // substr needs a length, not an end index
         const partLength = partEndIndex - partStartIndex;
-        const part = line.substr(partStartIndex, partLength);
+        let part = line.substr(partStartIndex, partLength);
+        if (isCurrentPartString && part.indexOf('\\') !== -1) {
+          // If the part has a backslash it's likely we have escaped quotes that we need to unescape (it's safe to always run this, but for performance best to only do when really necessary)
+          part = part.replace(/\\"/g, '"');
+        }
         parts.push(part);
 
         // Prepare next part
--- src/CombatLogFileReader.test.js
+++ src/CombatLogFileReader.test.js
@@ -21,7 +21,7 @@ describe('CombatLogFileReader', () => {
       ]);
     });
     it('splits lines with strings with escaped quotes in the strings properly', () => {
-      const raw = 'SPELL_AURA_APPLIED,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,207589,"Throw \"Stuff\"",0x2,BUFF';
+      const raw = 'SPELL_AURA_APPLIED,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,207589,"Throw \\"Stuff\\"",0x2,BUFF';
       expect(CombatLogFileReader.splitLine(raw)).toEqual([
         'SPELL_AURA_APPLIED',
         'Player-639-0650F657',
@@ -34,6 +34,24 @@ describe('CombatLogFileReader', () => {
         '0x0',
         '207589',
         'Throw "Stuff"',
+        '0x2',
+        'BUFF',
+      ]);
+    });
+    it('splits lines with strings with escaped quotes in the strings properly', () => {
+      const raw = 'SPELL_AURA_APPLIED,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,207589,"Throw \\"Stuff\\", or don\'t?",0x2,BUFF';
+      expect(CombatLogFileReader.splitLine(raw)).toEqual([
+        'SPELL_AURA_APPLIED',
+        'Player-639-0650F657',
+        'Sling-Al\'Akir',
+        '0x514',
+        '0x0',
+        'Player-639-0650F657',
+        'Sling-Al\'Akir',
+        '0x514',
+        '0x0',
+        '207589',
+        'Throw "Stuff", or don\'t?',
         '0x2',
         'BUFF',
       ]);
