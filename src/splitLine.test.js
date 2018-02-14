12449b2b5b8c5889d0cb277a7971b1f470e67405
Martijn Hols <martijnhols@hotmail.com>
2018-02-14 22:26:46 +0100

    Commented out benchmark test for future use

--- src/splitLine.test.js
+++ src/splitLine.test.js
@@ -73,4 +73,14 @@ describe('splitLine', () => {
       'BUFF',
     ]);
   });
+  // Benchmarking in a test is fun while doing on a single machine to determine what's quicker, but once you start running things on different environments this becomes pretty useless.
+  // it('runs quickly', () => {
+  //   const raw = 'SPELL_AURA_APPLIED,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,Player-639-0650F657,"Sling-Al\'Akir",0x514,0x0,207589,"Ilterendi, Crown Jewel of Silvermoon",0x2,BUFF';
+  //   const start = +new Date();
+  //   for (let i = 0; i < 300000; i++) {
+  //     splitLine(raw);
+  //   }
+  //   const duration = +new Date() - start;
+  //   expect(duration).toBeLessThan(1000);
+  // });
 });
