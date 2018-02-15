f927866543bcc7c6194b3f97568f38284da283e8
Martijn Hols <martijnhols@hotmail.com>
2018-02-15 22:29:12 +0100

    Some TODOs

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import { difficultyLabel } from 'common/DIFFICULTIES';
 import FightScanner from './FightScanner';
 import CombatLogFileReader from './CombatLogFileReader';
 
-// const entities = {};
-//
-// function processEntity(parts, startIndex) {
-//   const id = parts[startIndex + 0];
-//   const name = parts[startIndex + 1];
-//   const flags = parts[startIndex + 2];
-//   const flags2 = parts[startIndex + 3];
-//
-//   entities[id] = name;
-//
-//   return startIndex + 4;
-// }
-
 const reader = new CombatLogFileReader('WoWCombatLog.txt');
+// TODO: Read index
+// TODO: Validate index
+// TODO: Determine last scanned line
+// TODO: Offset scan by last scanned line
+// TODO: Store newly found fights in index
+// When all that is done fight scanning is finished.
+// TODO: Parse timestamps
+// Stage 1.2: parsing a single fight.
 const scanner = new FightScanner(reader);
 scanner.on('fight', fight => {
   console.log(`#${fight.startLineNo}-#${fight.endLineNo}`, `${fight.startTime}-${fight.endTime}`, fight.bossId, difficultyLabel(fight.difficulty), fight.bossName, fight.kill ? 'KILL' : 'WIPE');
